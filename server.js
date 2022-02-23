7221934e07302b045a212e1c8516cb5e26491707
Dan <danilobomfim.si@gmail.com>
2022-02-23 13:04:04 -0300

    Melhorando o retorno.

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const http = require("http");
+const host = "http://localhost";
+const port = 3000;
 
 http
   .createServer((req, res) => {
     res.end("<h1> Working </h1>");
   })
-  .listen(3000, () => console.log("Server is running"));
+  .listen(port, () => console.log(`Server is running on ${host}:${port}`));
